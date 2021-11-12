da9a675d826b1cfb41c2285aa667f9d52eaf1e18
Bruri Wijayanto <bruri@gi.co.id>
2021-11-12 15:49:58 +0700

    basic condition

---
+++ fundamental.py
@@ -0,0 +1,12 @@
+# SEQUENTIAL: Eksekusi berurutan
+print('Hello World!')
+print('this command')
+print('execute sequencial')
+print('-' * 10)
+
+# CONDITION
+is_danger = True
+if is_danger:
+    print('turn arround')
+else:
+    print('straight ahead')
